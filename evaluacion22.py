a490eb2ff52a76fa010c0387d1ada00b607e221b
Edward Brito Diaz <37183622+Edwardb11@users.noreply.github.com>
2020-11-10 20:51:40 -0400

    entrar

--- evaluacion22.py
+++ evaluacion22.py
@@ -6,24 +6,26 @@
 #2
 #Hacer una función recursiva que lea números enteros de teclado, hasta que el 
 #usuario ingrese el 0. Finalmente, mostrar la sumatoria de todos los números ingresados.
-
+def sumatoria():
+  acomulador=0
+  n=int(input("Introduzca un numero: "))
+  
 #3
 #Hacer una función recursiva que imprima los números primos de 0 a n. 
 
 def primos():
-    i=1
-    x = int(input("Enter the number:"))
-    for k in range (1, (x+1), 1):
-           c=0
-           for j in range (1, (i+1), 1):
-               a = i%j
-               if (a==0):
-                   c = c+1
+  inicializador=1
+  Primo = int(input("Enter the number:"))
+  for k in range (1, (Primo+1), 1):
+    c=0
+    for j in range (1, (Primo+1), 1):
+      a = inicializador%j
+      if (a==0):
+        c = c+1
+      if (c==2):
+        print (inicializador)
+      else:
+        k = k-1
 
-           if (c==2):
-                 print (i)
-           else:
-                 k = k-1
-
-           i=i+1
+        inicializador=inicializador+1
 primos()

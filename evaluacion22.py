89435775fba71017f45c85cfac4559b43404888b
Edward Brito Diaz <37183622+Edwardb11@users.noreply.github.com>
2020-11-12 09:53:10 -0400

    entrar

--- evaluacion22.py
+++ evaluacion22.py
@@ -5,13 +5,14 @@ def suma(n):
   if n==1:
     return 1
   else:
-    return n+suma(n-1)//suma(n) #Recursion 
+    return n+suma(n-1)
 
+def dividir(n):
+    return suma(n)/n
 
-#print(suma(5))    
-#n=int(input("Introduzca un numero: "))
-#print(f"La suma del numero es {suma(n)}")
-#print(f"La division de la suma numero es {suma(n/n)}")
+print(suma(5))
+print(dividir(5))    
+
 #2
 #Hacer una función recursiva que lea números enteros de teclado, hasta que el 
 #usuario ingrese el 0. Finalmente, mostrar la sumatoria de todos los números ingresados.
@@ -27,13 +28,29 @@ def sumatoria(acomulador=0):
 #sumatoria()
 #3
 #Hacer una función recursiva que imprima los números primos de 0 a n. 
-def primos(n):
-  losprimos=[]
-  for i in range(2,n):
-    if (n%i==0):
-      print("")
-    else:
-      losprimos.append( i)
-      return losprimos
-n=int(input("Indroduzca el limite de numero primos: "))
-print(primos(n))
+primos = []
+ 
+def es_primo(numero):
+    # Hacemos bucle desde 2 hasta el anterior al número
+    for i in range(2, numero):
+        # calculamos el resto. Si es cero => no es primo
+        if numero % i == 0:
+            return False
+    return True
+ 
+def calcularCantidadPrimosEnIntervalo(a, b):
+    if a < 2 or b < 2:
+        print ('Deben ser mayores que 1')
+        return False
+ 
+    if es_primo(a):
+        primos.append(a)
+ 
+    if a == b:
+        print ('Hemos encontrado: %s primos' % len(primos))
+        print (primos)
+        return
+ 
+    calcularCantidadPrimosEnIntervalo(a+1, b)
+ 
+#calcularCantidadPrimosEnIntervalo(2, 51)
